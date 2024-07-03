6602190ff58ed154a3457bbb3b12830fa4fafeec
byeongjun <pbj3810@kaist.ac.kr>
2024-07-03 12:52:23 +0900

    remove numerical instabilities in 3dgs

--- cuda_rasterizer/forward.cu
+++ cuda_rasterizer/forward.cu
@@ -342,14 +342,14 @@ renderCUDA(
 			// and its exponential falloff from mean.
 			// Avoid numerical instabilities (see paper appendix). 
 			float alpha = min(0.99f, con_o.w * exp(power));
-			if (alpha < 1.0f / 255.0f)
-				continue;
+// 			if (alpha < 1.0f / 255.0f)
+// 				continue;
 			float test_T = T * (1 - alpha);
-			if (test_T < 0.0001f)
-			{
-				done = true;
-				continue;
-			}
+// 			if (test_T < 0.0001f)
+// 			{
+// 				done = true;
+// 				continue;
+// 			}
 
 			// Eq. (3) from 3D Gaussian splatting paper.
 			for (int ch = 0; ch < CHANNELS; ch++)
